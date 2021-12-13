3f82f24f68f9578aa53df291ba1226a2258588d5
ChrisVaksdal <chris.vaksdla@gmail.com>
2021-12-13 16:27:37 +0100

    Added new solution for part 2 as old one was really slow.

--- 6/immortal.py
+++ 6/immortal.py
@@ -1,26 +1,28 @@
-import numpy as np
-
 INPUT_FILE_PATH = "input.txt"
 NUM_DAYS_PREGNANT = 7               # How many days before a lanternfish spawns a child.
 NUM_DAYS_INFERTILE = 2              # How many days before a lanternfish can reproduce.
 NUM_DAYS_TOTAL = 256                # How many days the observation will go on for.
 
-def cycleDay(lanterns):
-    lanterns -= 1
-    inLabor = np.where(lanterns == -1)
-    if inLabor[0].size > 0:
-        lanterns[inLabor] = NUM_DAYS_PREGNANT - 1
-        babyLanterns = np.array([NUM_DAYS_PREGNANT + NUM_DAYS_INFERTILE - 1] * len(inLabor[0]))
-        lanterns = np.append(lanterns, babyLanterns)
-    return lanterns
+NUM_STATES = NUM_DAYS_PREGNANT + NUM_DAYS_INFERTILE
+
 
 def main():
     with open(INPUT_FILE_PATH, "r") as File:
-        lanterns = np.array([int(fish) for fish in File.readline().split(",")])
-    for _ in range(NUM_DAYS_TOTAL):
-        lanterns = cycleDay(lanterns)
-    print("After %d days, there will be %d lanternfish in total." % (NUM_DAYS_TOTAL, len(lanterns)))
+        lanterns = [int(fish) for fish in File.readline().split(",")]
 
+    # Create a list containing how many lanternfish are in each state of gestation.
+    timerStates = [0] * NUM_STATES
+    for i in range(NUM_STATES):
+        timerStates[i] = lanterns.count(i)
+    
+    # Every day, move each element in the list one to the left. Add number in 0th position to both 7th and 9th positions.
+    for _ in range(NUM_DAYS_TOTAL):
+        newTimerStates = [timerStates[i + 1] for i in range(NUM_STATES - 1)]
+        newTimerStates[NUM_DAYS_PREGNANT - 1] += timerStates[0]
+        newTimerStates.append(timerStates[0])
+        timerStates = newTimerStates
+
+    print("After %d days, there will be %d lanternfish in total." % (NUM_DAYS_TOTAL, sum(timerStates)))
 
 
 if __name__ == "__main__":
